5b1fe12e353186fbbc2627c7a1743771b48fdf63
Oleksandr Solianyk <salym3k@gmail.com>
2023-01-24 19:46:22 +0200

    init project

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import PostCatalog from "./component/PostCatalog";
+
+function App() {
+    return (
+        <div className="App">
+            <PostCatalog/>
+        </div>
+    );
+}
+
+export default App;
